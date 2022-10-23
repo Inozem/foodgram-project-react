8b1434f2894d74cc393e32b87a889602800390c9
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2022-10-24 00:26:27 +0300

    Added filter is_in_shopping_cart

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -8,13 +8,24 @@ class RecipeFilterSet(FilterSet):
     author = NumberFilter(field_name='author__id')
     tags = CharFilter(field_name='tags__slug')
     is_favorited = NumberFilter(method='filter_is_favorited')
+    is_in_shopping_cart = NumberFilter(method='filter_shopping_cart')
 
     def filter_is_favorited(self, queryset, name, value):
+        user = self.request.user
         if value == 1:
-            return queryset.filter(favorite_recipe__user=self.request.user)
+            return queryset.filter(favorite_recipe__user=user)
         elif value == 0:
-            return queryset.exclude(favorite_recipe__user=self.request.user)
+            return queryset.exclude(favorite_recipe__user=user)
+        return queryset
+
+    def filter_shopping_cart(self, queryset, name, value):
+        user = self.request.user
+        if value == 1:
+            return queryset.filter(recipe_in_shopping_cart__user=user)
+        elif value == 0:
+            return queryset.exclude(recipe_in_shopping_cart__user=user)
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = ('author', 'tags', 'is_favorited',)
+        fields = ('author', 'tags', 'is_favorited', 'is_in_shopping_cart')
