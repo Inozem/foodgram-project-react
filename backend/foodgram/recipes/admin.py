34afea9e6bcc94903d6b4cb7be74303b2a72ed26
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2022-10-25 16:17:02 +0300

    Finished Admin panel

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 
 from recipes.models import Ingredient, Ingredients_amount, Recipe, Tag
 
-admin.site.register(Recipe)
+
+class RecipeAdmin(admin.ModelAdmin):
+    """Класс рецептов."""
+    list_display = ('name', 'author', 'get_favorites_count')
+    list_filter = ('author', 'name', 'tags')
+
+    def get_favorites_count(self, obj):
+        return obj.favorite_recipe.count()
+
+    get_favorites_count.short_description = (
+        'Кол-во человек добавивших в избранное'
+    )
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    """Класс ингредиентов."""
+
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Tag)
-admin.site.register(Ingredient)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Ingredients_amount)
