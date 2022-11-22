6862af99d4f7ebf2a0b4e664489a37dc25ec3d57
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2022-11-23 01:13:15 +0300

    Third code review: foodgram urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -1,8 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +9,3 @@ urlpatterns = [
 # if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL,
 #                          document_root=settings.MEDIA_ROOT)
-
