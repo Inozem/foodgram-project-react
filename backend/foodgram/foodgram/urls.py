092b817a1a1ff30606c52a7d29c261c007fcf5ce
Sergey Inozemtsev <inozemtsevs@yandex.ru>
2022-11-23 01:11:44 +0300

    Third code review: foodgram urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -7,21 +7,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-#    path(
-#        'redoc/',
-#        TemplateView.as_view(template_name='redoc.html'),
-#        name='redoc'
-#    ),
 ]
 
 # if settings.DEBUG:
 #    urlpatterns += static(settings.MEDIA_URL,
 #                          document_root=settings.MEDIA_ROOT)
 
-#urlpatterns += static(
-#    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-#)
-
-#urlpatterns += static(
-#    settings.STATIC_URL, document_root=settings.STATIC_ROOT
-#)
